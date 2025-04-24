76a031925581b977775114254bb1cd9cdd74d8b9
Alaitz Mendiola <amendiol@redhat.com>
2025-04-24 04:41:34 -0400

    added intentional failures

--- tests/system-tests/o-cloud/internal/ocloudcommon/day2-configuration.go
+++ tests/system-tests/o-cloud/internal/ocloudcommon/day2-configuration.go
@@ -30,6 +30,7 @@ import (
 //
 //nolint:funlen
 func VerifySuccessfulOperatorUpgrade(ctx SpecContext) {
+	Fail("forced failure for testing purposes")
 	downgradeOperatorImages()
 
 	provisioningRequest1 := VerifyProvisionSnoCluster(
@@ -145,6 +146,7 @@ func VerifySuccessfulOperatorUpgrade(ctx SpecContext) {
 //
 //nolint:funlen
 func VerifyFailedOperatorUpgradeAllSnos(ctx SpecContext) {
+	Fail("forced failure for testing purposes")
 	downgradeOperatorImages()
 
 	provisioningRequest1 := VerifyProvisionSnoCluster(
@@ -276,6 +278,7 @@ func VerifyFailedOperatorUpgradeAllSnos(ctx SpecContext) {
 //
 //nolint:funlen
 func VerifyFailedOperatorUpgradeSubsetSnos(ctx SpecContext) {
+	Fail("forced failure for testing purposes")
 	downgradeOperatorImages()
 
 	provisioningRequest1 := VerifyProvisionSnoCluster(
